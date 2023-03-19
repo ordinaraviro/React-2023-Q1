5438b8f61b89ac6977eb6dbf5f1d88039c3e6f21
Alexander_Yulin <ordinaraviro@gmail.com>
2023-03-19 12:22:33 +0200

    feat: add search bar

--- react-components/src/Pages/Main.tsx
+++ react-components/src/Pages/Main.tsx
@@ -1,15 +1,35 @@
+import SearchBar from '../components/SearchBar/SearchBar';
 import React from 'react';
 import MyComponent from '../api/apibtn';
 import Gallery from '../components/Gallery/Gallery';
 
-function Main() {
+interface MainState {
+  searchTerm: string;
+}
+
+class Main extends React.Component<{}, MainState> {
+  constructor(props: MainState) {
+    super(props);
+    this.state = {
+      searchTerm: "hoverla",
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(searchTerm: string) {
+    this.setState({ searchTerm });
+  }
+
+  render() {
     return (
-        <div>
-            Main
-            <MyComponent/>
-            <Gallery searchText='a' perPage={10}/>
-        </div>
-    )
+      <div>
+        Main
+        <MyComponent />
+        <SearchBar onSearch={this.handleSearch} />
+        <Gallery searchText={this.state.searchTerm} perPage={10} />
+      </div>
+    );
+  }
 }
 
 export default Main;
--- react-components/src/components/Gallery/Gallery.tsx
+++ react-components/src/components/Gallery/Gallery.tsx
@@ -12,47 +12,59 @@ interface State {
 }
 
 class Gallery extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      data: null,
-    };
-  }
-
-  async componentDidMount() {
-    const { searchText, perPage } = this.props;
-    const data = await fetchFlickrData('nature', 20);
-    this.setState({ data });
-  }
-
-  render() {
-    const { data } = this.state;
-
-    if (!data) {
-      return <div>Loading...</div>;
+    constructor(props: Props) {
+      super(props);
+      this.state = {
+        data: null,
+      };
+      this.fetchData = this.fetchData.bind(this);
     }
-
-    const photos = data.photos.photo;
-
-    return (
-      <div className="gallery">
-        {photos.map((photo) => (
-          <div className="card" key={photo.id}>
-            <img
-              src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_n.jpg`}
-              alt={photo.title}
-            />
-            <div className="card-body">
-              <h5 className="card-title">{photo.title}</h5>
-              <p className="card-text">
-                <small className="text-muted">Views: {photo.views}</small>
-              </p>
+  
+    async componentDidMount() {
+      this.fetchData();
+    }
+  
+    async componentDidUpdate(prevProps: Props) {
+      if (prevProps.searchText !== this.props.searchText) {
+        this.fetchData();
+      }
+    }
+  
+    async fetchData() {
+      const { searchText, perPage } = this.props;
+      const data = await fetchFlickrData(searchText, perPage);
+      this.setState({ data });
+    }
+  
+    render() {
+      const { data } = this.state;
+  
+      if (!data) {
+        return <div>Loading...</div>;
+      }
+  
+      const photos = data.photos.photo;
+  
+      return (
+        <div className="gallery">
+          {photos.map((photo) => (
+            <div className="card" key={photo.id}>
+              <img
+                src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_n.jpg`}
+                alt={photo.title}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{photo.title}</h5>
+                <p className="card-text">
+                  <small className="text-muted">Views: {photo.views}</small>
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    );
+          ))}
+        </div>
+      );
+    }
   }
-}
+  
 
 export default Gallery;
