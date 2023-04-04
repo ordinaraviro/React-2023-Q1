2ef0069bd25bc50c9952badd2924ca670be147cb
Alexander_Yulin <ordinaraviro@gmail.com>
2023-04-04 06:48:18 +0300

    test: add page 404 test

--- react-components/vite.config.ts
+++ react-components/vite.config.ts
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import sass from 'sass'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'c8',
+      all: true
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
