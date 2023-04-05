0fc7c3f26992ae85b90730a3bcd4daab4d3bd493
Alexander_Yulin <ordinaraviro@gmail.com>
2023-04-05 17:44:17 +0300

    feat: add style for form

--- react-components/src/components/Form/Form.tsx
+++ react-components/src/components/Form/Form.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import ConfirmationModal from './ConfirmationModal';
+import './Form.scss';
 
 interface FormData {
   name: string;
@@ -16,7 +18,13 @@ interface FormProps {
 }
 
 const Form: React.FC<FormProps> = ({ onSubmit }) => {
-  const { register, handleSubmit, reset } = useForm<FormData>();
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const submitHandler = (formData: FormData) => {
     if (formData.file && formData.file.length > 0) {
@@ -25,56 +33,70 @@ const Form: React.FC<FormProps> = ({ onSubmit }) => {
     }
     onSubmit(formData);
     reset();
+    setShowConfirmation(true);
+  };
+
+  const handleConfirmationClose = () => {
+    setShowConfirmation(false);
   };
 
   return (
-    <form onSubmit={handleSubmit(submitHandler)}>
-      <label>
-        Title:
-        <input type="text" {...register('name')} />
-      </label>
-      <br />
-      <label>
-        Date:
-        <input type="date" {...register('date')} />
-      </label>
-      <br />
-      <label>
-        Category:
-        <select {...register('option')}>
-          <option value="">--Select--</option>
-          <option value="Option 1">People</option>
-          <option value="Option 2">Nature</option>
-          <option value="Option 3">City</option>
-        </select>
-      </label>
-      <br />
-      <label>
-        Checkboxes:
+    <div>
+      <form className="form" onSubmit={handleSubmit(submitHandler)}>
+        <label>
+          Title:
+          <input type="text" {...register('name', { required: true })} />
+          {errors.name && <span>This field is required</span>}
+        </label>
         <br />
-        <input type="checkbox" {...register('checkboxes.0')} />
-        <label>Checkbox 1</label>
+        <label>
+          Date:
+          <input type="date" {...register('date', { required: true })} />
+          {errors.date && <span>This field is required</span>}
+        </label>
         <br />
-        <input type="checkbox" {...register('checkboxes.1')} />
-        <label>Checkbox 2</label>
+        <label>
+          Category:
+          <select {...register('option', { required: true })}>
+            <option value="">--Select--</option>
+            <option value="People">People</option>
+            <option value="Nature">Nature</option>
+            <option value="City">City</option>
+          </select>
+          {errors.option && <span>This field is required</span>}
+        </label>
         <br />
-        <input type="checkbox" {...register('checkboxes.2')} />
-        <label>Checkbox 3</label>
-      </label>
-      <br />
-      <label>
-        Access:
-        <input type="radio" {...register('switcher')} value="On" /> Private
-        <input type="radio" {...register('switcher')} value="Off" /> Public
-      </label>
-      <br />
-      <label>
-        Image:
-        <input type="file" {...register('file')} />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+        <label>
+          Tags:
+          <br />
+          <input type="checkbox" {...register('checkboxes')} value="Wallpaper " />
+          <label>Wallpaper</label>
+          <br />
+          <input type="checkbox" {...register('checkboxes')} value="Beautiful " />
+          <label>Beautiful</label>
+          <br />
+          <input type="checkbox" {...register('checkboxes')} value="Art " />
+          <label>Art</label>
+          {errors.checkboxes && <span>This field is required</span>}
+        </label>
+        <br />
+        <label>
+          Access:
+          <input type="radio" {...register('switcher', { required: true })} value="On" /> Private
+          <input type="radio" {...register('switcher', { required: true })} value="Off" /> Public
+          {errors.switcher && <span>This field is required</span>}
+        </label>
+        <br />
+        <label>
+          Image:
+          <input type="file" {...register('file', { required: true })} />
+          {errors.file && <span>This field is required</span>}
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <ConfirmationModal show={showConfirmation} onClose={handleConfirmationClose} />
+    </div>
   );
 };
 
--- react-components/src/components/Form/FormCards.tsx
+++ react-components/src/components/Form/FormCards.tsx
@@ -19,17 +19,23 @@ const FormCards: React.FC<Props> = ({ cards }) => {
         <div>
           <h2>Cards:</h2>
           {cards.map((card, index) => (
-            <div key={index}>
-              <h3>{card.name}</h3>
-              <p>{card.date}</p>
-              <p>{card.option}</p>
-              {card.checkboxes.map((checkbox, checkboxIndex) => (
-                <p key={checkboxIndex}>
-                  <input type="checkbox" checked={checkbox} disabled />
-                  {checkbox ? 'Checked' : 'Unchecked'}
-                </p>
-              ))}
-              <p>{card.switcher}</p>
+            <div key={index} className="card">
+              <h3>Title: {card.name}</h3>
+              <p>Date: {card.date}</p>
+              <p>Category: {card.option}</p>
+              <p>
+                Tags:{' '}
+                {card.checkboxes.map((checkbox, checkboxIndex) => {
+                  if (checkbox) {
+                    return (
+                      <span key={checkboxIndex} className="badge bg-secondary me-1">
+                        {card.checkboxes[checkboxIndex]}
+                      </span>
+                    );
+                  }
+                })}
+              </p>
+              <p>Access: {card.switcher ? 'Private' : 'Public'}</p>
               {card.file && <img src={card.fileUrl} alt="Selected file" />}
             </div>
           ))}
