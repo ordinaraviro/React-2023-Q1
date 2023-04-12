98fc0e34e1e05b2f12da443c95556471015e4ede
Alexander_Yulin <ordinaraviro@gmail.com>
2023-04-12 08:46:13 +0300

    fix: empty search bar

--- react-components/src/api/api.ts
+++ react-components/src/api/api.ts
@@ -31,9 +31,13 @@ interface PhotosResponse {
 const apiKey = '9a1e3efb17626c41c96f0d55637ab401';
 
 async function fetchData(searchText: string, perPage: number): Promise<PhotosResponse> {
-  const response = await fetch(
-    `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchText}&per_page=${perPage}&extras=views,description,date_upload,date_taken,owner_name,last_update,geo,url_o&format=json&nojsoncallback=1`
-  );
+  const response = searchText.trim()
+    ? await fetch(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchText}&per_page=${perPage}&extras=views,description,date_upload,date_taken,owner_name,last_update,geo,url_o&format=json&nojsoncallback=1`
+      )
+    : await fetch(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${apiKey}&per_page=${perPage}&extras=views,description,date_upload,date_taken,owner_name,last_update,geo,url_o&format=json&nojsoncallback=1`
+      );
   const data = await response.json();
   return data;
 }
--- react-components/src/components/Gallery/Gallery.tsx
+++ react-components/src/components/Gallery/Gallery.tsx
@@ -18,12 +18,15 @@ const Gallery: React.FC<Props> = ({ searchText, perPage }) => {
       const result = await fetchData(searchText, perPage);
       setData(result);
     };
-
+    setData(null);
     fetchDataAsync();
   }, [searchText, perPage]);
 
   if (!data) {
     return <div>Loading...</div>;
+  }
+  if (!data.photos.photo.length) {
+    return <div>Sorry, nothing found</div>;
   }
 
   const photos = data.photos.photo;
